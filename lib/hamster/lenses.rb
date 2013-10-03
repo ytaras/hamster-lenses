0192a4a82410ce86e653ba944fdfe61c70e18f7b
Yura Taras <yura.taras@gmail.com>
2013-10-04 01:11:35 +0300

    Moved TODO's

--- lib/hamster/lenses.rb
+++ lib/hamster/lenses.rb
@@ -2,3 +2,6 @@ require "hamster/lenses/version"
 require "hamster/lenses/lense"
 require "hamster/lenses/hash"
 require "hamster/lenses/composed"
+
+# TODO @ytaras Lense convenience DSL
+# TODO @ytaras List lenses
--- todo.rb
+++
@@ -1,4 +0,0 @@
-# TODO @ytaras HashMap lense
-# TODO @ytaras compose lenses
-# TODO @ytaras Lense convenience DSL
-# TODO @ytaras List lenses
