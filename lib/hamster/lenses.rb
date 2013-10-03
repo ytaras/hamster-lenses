fdd70a70029b911a4fa72a7cf892c0bfc9c90572
Yura Taras <yura.taras@gmail.com>
2013-10-03 03:35:12 +0300

    Initial commit

---
+++ lib/hamster/lenses.rb
@@ -0,0 +1,7 @@
+require "hamster/lenses/version"
+
+module Hamster
+  module Lenses
+    # Your code goes here...
+  end
+end
---
+++ lib/hamster/lenses/version.rb
@@ -0,0 +1,5 @@
+module Hamster
+  module Lenses
+    VERSION = "0.0.1"
+  end
+end
