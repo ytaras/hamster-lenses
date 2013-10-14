29fced5fede7a90f70d8de231a4f7404f929fc85
Yura Taras <yura.taras@gmail.com>
2013-10-14 23:12:18 +0300

    All list lense

--- lib/hamster/lenses/all_list.rb
+++ lib/hamster/lenses/all_list.rb
@@ -1,13 +1,55 @@
 require "hamster/list"
 require "singleton"
 
-class Hamster::Lenses::AllList
-  include Singleton
-  include Hamster::Lenses::Lense
+module Hamster::Lenses
+  class AllList
+    include Singleton
+    include Hamster::Lenses::Lense
 
-  def get(list)
-    check_not_nil(list)
-    list
+    def get(list)
+      check_not_nil(list)
+      list
+    end
+
+    def put(list, value = Hamster::Undefined, &block)
+      check_not_nil(list)
+      list.map do |x|
+        if value == Hamster::Undefined
+          block.call(x)
+        else
+          value
+        end
+      end
+    end
+
+    def and_then(other)
+      AllListComposed.new(self, other)
+    end
+
+  end
+
+  class AllListComposed
+    include Lense
+
+    attr_reader :lense1, :lense2
+
+    def initialize(lense1, lense2)
+      check_not_nil(lense1)
+      check_not_nil(lense2)
+      @lense1 = lense1
+      @lense2 = lense2
+    end
+
+    def get(object)
+      lense1.get(object).map { |x| lense2.get(x) }
+    end
+
+    def put(object, value = Hamster::Undefined, &block)
+      lense1.put(object) do |inner|
+        lense2.put(inner, value, &block)
+      end
+    end
+
   end
 
 end
--- test/lib/hamster/lenses/all_list_test.rb
+++ test/lib/hamster/lenses/all_list_test.rb
@@ -2,18 +2,52 @@ require_relative '../../../test_helper'
 require "hamster/list"
 
 describe Hamster::Lenses::List do
-  subject { Hamster::Lenses }
+  subject { Hamster::Lenses.all_list }
   before do
     @list = Hamster.list(*%i(one two three))
   end
 
   it 'must get all values' do
-    subject.all_list.get(@list).must_equal @list
+    subject.get(@list).must_equal @list
   end
 
-  it 'must compose with hash on get'
-  it 'must put all values'
-  it 'must put all values with block'
-  it 'must compose with hash on put'
+  it 'must put all values' do
+    subject.put(@list, :value).must_equal Hamster
+      .list *(3.times.map { :value})
+  end
+
+  it 'must put all values with block' do
+    subject.put(@list, &:to_s).must_equal @list.map(&:to_s)
+  end
+
+  describe 'composed with hash' do
+    before do
+      @deep_list = @list.map { |x| Hamster.hash(key: x) }
+    end
+
+    it 'must get' do
+      subject[:key].get(@deep_list).must_equal @list
+    end
+
+    it 'must put' do
+      subject[:key].put(@deep_list, :value).must_equal @list.map {
+        Hamster.hash(key: :value)
+      }
+    end
+
+    it 'must put with block' do
+      subject[:key].put(@deep_list, &:to_s).must_equal @list.map { |x|
+        Hamster.hash(key: x.to_s)
+      }
+    end
+
+  end
+
+  describe 'composed in a complex way' do
+    it 'must get'
+    it 'must put'
+    it 'must put with block'
+  end
+
 
 end
